4915c5ca266f3d87d1360ab57e1bd1e4c3e679b4
hasbi1028 <hasbi.awal@gmail.com>
2024-07-28 08:36:44 +0700

    configurasi auth controller dan komentar penjelasannya

--- mts-digital/app/Http/Controllers/AuthController.php
+++ mts-digital/app/Http/Controllers/AuthController.php
@@ -10,23 +10,29 @@ use App\Http\Resources\UserResource;
 
 class AuthController extends Controller
 {
+    // Metode login
     public function login(Request $request)
     {
+        // Validasi input
         $request->validate([
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
+        // Mencari user berdasarkan email
         $user = User::where('email', $request->email)->first();
 
+        // Memeriksa kecocokan password
         if (! $user || ! Hash::check($request->password, $user->password)) {
             return response()->json([
                 'message' => 'The provided credentials are incorrect.'
             ], 401);
         }
 
+        // Membuat token
         $token = $user->createToken('auth_token')->plainTextToken;
 
+        // Mengembalikan response
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
@@ -34,10 +40,13 @@ class AuthController extends Controller
         ]);
     }
 
+    // Metode logout
     public function logout(Request $request)
     {
+        // Menghapus token saat ini
         $request->user()->currentAccessToken()->delete();
 
+        // Mengembalikan response
         return response()->json([
             'message' => 'Successfully logged out',
         ]);
