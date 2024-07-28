ee6df471b5bae916921c60443437493d04a0c6a8
hasbi1028 <hasbi.awal@gmail.com>
2024-07-28 10:14:02 +0700

    menambahkan index tabel user

--- mts-digital/routes/api.php
+++ mts-digital/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserContoller;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +23,5 @@ Route::post('/register', [AuthController::class, 'register']);
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    Route::get('/user',[UserContoller::class,'index']);
 });
