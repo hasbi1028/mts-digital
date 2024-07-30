e67dcd0247baf0d5aee4382bce1e314feb184a7a
hasbi1028 <hasbi.awal@gmail.com>
2024-07-30 07:01:43 +0700

    kembali ke laptop

--- mts-digital/routes/api.php
+++ mts-digital/routes/api.php
@@ -3,12 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserContoller;
-
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\StudentDetailController;
-use App\Http\Controllers\StudentAddressController;
-use App\Http\Controllers\StudentParentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,14 +19,10 @@ use App\Http\Controllers\StudentParentController;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user',[UserContoller::class,'index']);
-    Route::apiResource('students', StudentController::class);
-Route::apiResource('student-details', StudentDetailController::class);
-Route::apiResource('student-addresses', StudentAddressController::class);
-Route::apiResource('student-parents', StudentParentController::class);
-    
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
